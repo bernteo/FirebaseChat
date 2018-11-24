65b2ba07e8a70a514b7a364a949e85f61fa0b7ce
bernteo <bernteo@gmail.com>
2018-11-24 11:21:08 +0800

    Added registerButton with constraints.

--- FirebaseChat/Controller/LoginController.swift
+++ FirebaseChat/Controller/LoginController.swift
@@ -20,14 +20,27 @@ class LoginController: UIViewController {
         return ivc
     }()
     
+    //registerButton : UIButton!
+    let registerButton : UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor(r: 80, g: 101, b: 161)
+        button.setTitle("Register", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
         
         view.addSubview(inputsCV)
+        view.addSubview(registerButton)
         
         setupInputsCV()
+        setupRegisterButton()
     }
     
     func setupInputsCV() {
@@ -36,7 +49,14 @@ class LoginController: UIViewController {
         inputsCV.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
         inputsCV.heightAnchor.constraint(equalToConstant: 150).isActive = true
     }
-    ≥
+    
+    func setupRegisterButton() {
+        registerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        registerButton.topAnchor.constraint(equalTo: inputsCV.bottomAnchor, constant: 12).isActive = true
+        registerButton.widthAnchor.constraint(equalTo: inputsCV.widthAnchor).isActive = true
+        registerButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
